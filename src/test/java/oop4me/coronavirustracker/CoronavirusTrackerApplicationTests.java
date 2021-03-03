193ced9e1cd9812e488727388ad58dd3582eee2d
GeraldRyan <gerald.ryan40@protonmail.com>
2021-03-03 01:22:44 -0600

    imported files

---
+++ src/test/java/oop4me/coronavirustracker/CoronavirusTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package oop4me.coronavirustracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronavirusTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
